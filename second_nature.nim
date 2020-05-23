e68d202a81153d278e0b37bc636e669b30aec09d
Buster Blue <buster_blue@protonmail.com>
2020-05-23 17:01:52 -0400

    Bring Nim version to parity with Python version

--- second_nature.nim
+++ second_nature.nim
@@ -1,1 +1,14 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
+import os
+
+
+proc game_reset() =
+    var food = 10
+    var population = 5
+    var housing = 2
+
+echo("Second Nature")
+sleep(3000)
+echo("Reach a population of 20 to win")
+sleep(3000)
+game_reset()
