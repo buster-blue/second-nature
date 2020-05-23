ad8e087c52377a6e8dfe571631014e398a603f79
Buster Blue <buster_blue@protonmail.com>
2020-05-23 16:31:39 -0400

    Started work on game_reset function and game intro

--- second_nature.py
+++ second_nature.py
@@ -1,1 +1,12 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
+import time
+
+
+def game_reset():
+    colony_food = 10
+    colony_pop = 5
+    colony_housing = 2
+
+
+print("Second Nature")
+time.sleep(3)
