160791a4a1aa83209481ff59e27c56d319fea706
Buster Blue <buster_blue@protonmail.com>
2020-05-23 17:38:31 -0400

    Start making the main game-loop

--- second_nature.py
+++ second_nature.py
@@ -1,15 +1,38 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 import time
 
+# Game variables
+food = 10
+population = 5
+housing = 2
+
 
 def game_reset():
     """reset the game"""
-    food = 10
     population = 5
+    food = 10
     housing = 2
 
 
 print("Second Nature")
 time.sleep(3)
-print("Reach a population of 20 to win")
+print("Reach a population of 20 to win\n")
 time.sleep(3)
+game_reset()
+
+
+# Main game loop
+while population > 0 and population < 20:
+    print("Colony Status:")
+    print(f"population: {population}\n" f"food: {food}\n" f"housing: {housing}\n")
+    print("1. Activate\n" "2. Move\n" "3. Build")
+    player_choice = input()
+    if player_choice == "1":
+        # Activate what?
+        pass
+    if player_choice == "2":
+        # from where -> to where
+        pass
+    if player_choice == "3":
+        # build what?
+        pass
