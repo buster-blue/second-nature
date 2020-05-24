1ca30cb4cf37fb5186c9316a635f68206f1fb744
Buster Blue <buster_blue@protonmail.com>
2020-05-24 12:48:47 -0400

    Add more variables and work on game-loop more

--- second_nature.py
+++ second_nature.py
@@ -2,16 +2,26 @@
 import time
 
 # Game variables
-food = 10
+number_of_forcasters = 0
+number_of_farmers = 0
+number_of_harvesters = 0
+number_of_resters = 0
+receiver_charge = 0
+amount_of_food = 10
 population = 5
-housing = 2
+number_of_houses = 2
+number_of_food_silos = 1
+food_cap = number_of_food_silos * 5
+pop_cap = number_of_houses * 3
+disaster_threshold = 3 + (number_of_forcasters * 2)
+amount_of_material = 0
 
 
 def game_reset():
     """reset the game"""
     population = 5
     food = 10
-    housing = 2
+    houses = 2
 
 
 print("Second Nature")
@@ -24,7 +34,11 @@ game_reset()
 # Main game loop
 while population > 0 and population < 20:
     print("Colony Status:")
-    print(f"population: {population}\n" f"food: {food}\n" f"housing: {housing}\n")
+    print(
+        f"population: {population}\n"
+        f"food: {amount_of_food}\n"
+        f"houses: {number_of_houses}\n"
+    )
     print("1. Activate\n" "2. Move\n" "3. Build")
     player_choice = input()
     if player_choice == "1":
@@ -35,4 +49,7 @@ while population > 0 and population < 20:
         pass
     if player_choice == "3":
         # build what?
+        print("1. Food Silo\n" "2. House")
         pass
+    # feed colonists
+    # disaster chance
